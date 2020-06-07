f4d3577c7339712a81dbc23a1350f6729d5cc09c
CyberSrikanth <ksksrikanthksk@gmail.com>
2020-06-07 13:30:04 +0530

    added chdir in start script

--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+cd ~/.passman-v2
 if [ $1 = "push" ];
 then
-~/.passman-v2/env/bin/python backup_man.py push
+./env/bin/python backup_man.py push
 elif [ $1 = "pull" ];
 then
-~/.passman-v2/env/bin/python backup_man.py pull
+./env/bin/python backup_man.py pull
 elif [ $1 = "signout" ];
 then
 rm token.pickle
 else
-~/.passman-v2/env/bin/python init.py
+./env/bin/python init.py
 fi
